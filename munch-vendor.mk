8e3fb6209575622169e26fe62f02012a4ce1dde0
failover4nix <failover4nix@gmail.com>
2025-11-14 01:50:09 +0000

    remove xtra-daemon download a-gps file from qualcomm servers and cause leaks of cpu and modem id. gps tested and works fine without a-gps data. tff between 30s and 2min isn't a bad resault for privacy.

--- munch-vendor.mk
+++ munch-vendor.mk
@@ -266,7 +266,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/munch/proprietary/vendor/etc/thermald-devices.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermald-devices.conf \
     vendor/xiaomi/munch/proprietary/vendor/etc/wifi/aoa_cldb_falcon.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/aoa_cldb_falcon.bin \
     vendor/xiaomi/munch/proprietary/vendor/etc/wifi/aoa_cldb_swl14.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/aoa_cldb_swl14.bin \
-    vendor/xiaomi/munch/proprietary/vendor/etc/xtra_root_cert.pem:$(TARGET_COPY_OUT_VENDOR)/etc/xtra_root_cert.pem \
     vendor/xiaomi/munch/proprietary/vendor/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf \
     vendor/xiaomi/munch/proprietary/vendor/firmware/0_click_P_RTP.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/0_click_P_RTP.bin \
     vendor/xiaomi/munch/proprietary/vendor/firmware/1_doubelClick_P_RTP.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/1_doubelClick_P_RTP.bin \
@@ -1068,7 +1067,6 @@ PRODUCT_PACKAGES += \
     time_daemon \
     toucheventcheck \
     vppservice \
-    xtra-daemon \
     xtwifi-client \
     xtwifi-inet-agent \
     dpmd
